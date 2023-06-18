214cbc10b3d2c114a5bea37c8f9d5aa30553d7a2
Coding Marquice <49505193+GoldenMarquice@users.noreply.github.com>
2023-06-18 12:40:16 -0600

    Add files via upload

--- TaskManager/scripts/app.js
+++ TaskManager/scripts/app.js
@@ -34,25 +34,45 @@ function toggleImportant() {
     console.log("Icon Clicked!")
 }
 
-function saveTask() {
+async function saveTask() {
 
-    let title= $("#txtTitle").val();
+    let title = $("#txtTitle").val();
     let description = $("#txtDescription").val();
     let dueDate = $("#selDueDate").val();
     let status = $("#selStatus").val();
     let budget = $("#txtBudget").val();
     let color = $("#selColor").val();
 
-    let taskSave = new Task(isImportant, title,description,dueDate,status,budget,color);
+    let taskSave = new Task(isImportant, title, description, dueDate, status, budget, color);
 
-    displayTask(taskSave);
-    clearForm();
-    
+    //send Task to the server
+    let response = await fetch("https://fsdiapi.azurewebsites.net/api/tasks/", {
+        method: "Post",
+        body: JSON.stringify(taskSave), // makes objects into a string to process http request
+        headers: {
+            "Content-type": "application/json"
+        }
+    });
+
+    if (response.ok) {
+        displayTask(taskSave);
+        clearForm();
+
+        // get data from the response
+        let data = await response.json();
+        console.log(data);
+
+    } else {
+        alert("Error saving tasks, please try again.")
+    }
+
+
+
 }
 
-function displayTask(task){
+function displayTask(task) {
     let syntax = `
-    <div class='tasks'> 
+    <div class='tasks' style="border-color:${task.color}"> 
     <div class= 'details'>
     <h5> ${task.title}</h5>
     <p> ${task.description}</p>
@@ -67,17 +87,57 @@ function displayTask(task){
     $("#pending-tasks").append(syntax);
 }
 
-function clearForm(){
+function clearForm() {
     $("#txtTitle").val("");
     $("#txtDescription").val("");
     $("#selDueDate").val("");
     $("#selStatus").val("");
     $("#txtBudget").val("");
     $("#selColor").val("");
-    
+
 
 
 }
+
+async function testRequest() {
+    let response = await fetch("https://fsdiapi.azurewebsites.net/"); //Server Request
+    console.log(response)
+}
+
+async function loadTasks() {
+    // get https://fsdiapi.azurewebsites.net/api/tasks/
+    // console log the data from the response
+    let response = await fetch("https://fsdiapi.azurewebsites.net/api/tasks");
+
+    if (response.ok) {
+        let data = await response.json();
+
+        for (let i = 0; i < data.length; i++) {
+            let task = data[i];
+            if (task.name == "Marquice") {
+                displayTask(task)
+            }
+        }
+
+    } else {
+        alert("Error no task register")
+    }
+}
+
+async function deleteAll(){
+    let response = await fetch("https://fsdiapi.azurewebsites.net/api/tasks/clear/Marquice/", {
+        method: "DELETE"
+    });
+
+    if (response.ok){
+        // remove all tasks from the screen
+        $(".tasks").remove();
+    }else{
+        alert("Error delete your tasks");
+    }
+    console.log(response);
+}
+
 
 
 
@@ -85,8 +145,11 @@ function init() {
     console.log("Task Manager");
 
     //load data
+    loadTasks();
 
     //catch or hook events
+
+    $("#deleteAll").click(deleteAll);
 
     $("#hideDetails").click(hiddenDetails);
 
